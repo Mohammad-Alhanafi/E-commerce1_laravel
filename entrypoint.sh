31d884544c96fb38ff48a1f679491cdac30abdba
Mohammad Alhanafi <m.alhanafi2003@gmail.com>
2026-08-10 14:29:28 +0300

    Add cache clear and force migrate in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# تنفيذ الـ Migration عند كل إطلاق للحاوية
+# 1. تنظيف كاش لارافيل بالكامل
+php artisan config:clear
+php artisan cache:clear
+
+# 2. تشغيل الـ Migration لإنشاء الجداول في PostgreSQL
 php artisan migrate --force
 
-# تشغيل سيرفر Apache الأساسي
+# 3. تشغيل Apache
 exec apache2-foreground
